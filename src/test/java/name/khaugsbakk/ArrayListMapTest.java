b90c1a1ce9eee3e9983bd28d69aa781660703422
Kristoffer Haugsbakk <code@khaugsbakk.name>
2023-01-15 20:24:03 +0100

    Clean up

--- src/test/java/name/khaugsbakk/ArrayListMapTest.java
+++ src/test/java/name/khaugsbakk/ArrayListMapTest.java
@@ -1,7 +1,8 @@
 package name.khaugsbakk;
 
-import org.apache.commons.lang3.builder.ToStringExclude;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ArrayListMapTest {
 
@@ -9,21 +10,21 @@ public class ArrayListMapTest {
     public void addOne() {
         var dict = new ArrayListMap<Integer, String>();
         dict.insert(1, "One");
-        org.junit.jupiter.api.Assertions.assertEquals(dict.get(1).get(), "One");
+        assertEquals(dict.get(1).get(), "One");
     }
 
     public void overwriteOne() {
         var dict = new ArrayListMap<Integer, String>();
         dict.insert(1, "One");
         dict.insert(1, "Uno");
-        org.junit.jupiter.api.Assertions.assertEquals(dict.get(1).get(), "Uno");
+        assertEquals(dict.get(1).get(), "Uno");
     }
 
     public void addTwo() {
         var dict = new ArrayListMap<Integer, String>();
         dict.insert(1, "One");
         dict.insert(2, "Two");
-        org.junit.jupiter.api.Assertions.assertEquals(dict.get(1).get(), "One");
-        org.junit.jupiter.api.Assertions.assertEquals(dict.get(1).get(), "Two");
+        assertEquals(dict.get(1).get(), "One");
+        assertEquals(dict.get(1).get(), "Two");
     }
 }
