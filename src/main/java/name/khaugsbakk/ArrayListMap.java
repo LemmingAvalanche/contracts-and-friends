a65cacf8c05bc19a82fa8e2dae4a5f479a5fa27e
Kristoffer Haugsbakk <code@khaugsbakk.name>
2023-01-17 20:28:50 +0100

    Iter

--- src/main/java/name/khaugsbakk/ArrayListMap.java
+++ src/main/java/name/khaugsbakk/ArrayListMap.java
@@ -3,9 +3,12 @@ package name.khaugsbakk;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Optional;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
-public class ArrayListMap<K, V> implements Dictionary<K, V> {
+public class ArrayListMap<K, V> implements Dictionary<K, V>, Iterable<Pair<K, V>> {
 
     private final ArrayList<Pair<K, V>> list = new ArrayList<>();
 
@@ -47,4 +50,30 @@ public class ArrayListMap<K, V> implements Dictionary<K, V> {
         return -1;
     }
 
+    @Override
+    public Iterator<Pair<K, V>> iterator() {
+        return new CustomIterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super Pair<K, V>> action) {
+        Iterable.super.forEach(action);
+    }
+
+    @Override
+    public Spliterator<Pair<K, V>> spliterator() {
+        return Iterable.super.spliterator();
+    }
+
+    class CustomIterator implements Iterator<Pair<K, V>> {
+        @Override
+        public boolean hasNext() {
+            return list.iterator().hasNext();
+        }
+
+        @Override
+        public Pair<K, V> next() {
+            return list.iterator().next();
+        }
+    }
 }
--- src/main/java/name/khaugsbakk/HashMap.java
+++ src/main/java/name/khaugsbakk/HashMap.java
@@ -10,10 +10,27 @@ public class HashMap<K, V> implements Dictionary<K, V> {
 
     private static final int SIZE = 8;
 
+    private static final float loadFactor = 0.7f;
+
     ArrayList<ArrayListMap<K, V>> map = new ArrayList<>();
     {
         for (int i = 0; i < SIZE; i++) {
             map.add(new ArrayListMap<>());
+        }
+    }
+
+    private void resize() {
+        var oldMap = map;
+        int newCapacity = map.size() * 2;
+        var newMap = new ArrayList<ArrayListMap<K, V>>(newCapacity);
+        this.map = newMap;
+        for (int i = 0; i < newCapacity; i++) {
+            newMap.add(new ArrayListMap<>());
+        }
+        for (var subMap : oldMap) {
+            for (var pair : subMap) {
+                insert(pair.getKey(), pair.getValue());
+            }
         }
     }
 
@@ -26,6 +43,9 @@ public class HashMap<K, V> implements Dictionary<K, V> {
 
     @Override
     public void insert(K key, V value) {
+        if (((float) size()) / ((float) map.size()) > loadFactor) {
+            resize();
+        }
         int preSize = size();
 
         int hash = key.hashCode();
